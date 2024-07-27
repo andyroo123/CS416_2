841a8f52b5bcf1a17a5045bad85e882f5e560385
Andrew Kolarits <amkolarits0222@gmail.com>
2024-07-27 08:21:42 -0400

    asdf

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:04527a60f1aae55152aa6fb2f2cf2d1261a6492a53281ff589b0e29daf732f05
-size 128
+import { select } from d3;
+
+const svg = select('svg');
+const width = +svg.attr("width");
+const height = +svg.attr("Height");
